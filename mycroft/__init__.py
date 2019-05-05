4cbc2b1c888d95f04eb9e608e2f8ecdff7299347
Nguyễn Hùng <nguyenhungsync@gmail.com>
2019-05-05 11:41:19 +0700

    Update __init__.py

--- mycroft/__init__.py
+++ mycroft/__init__.py
@@ -14,7 +14,7 @@
 #
 from os.path import abspath, dirname, join
 
-from mycroft.api import Api
+#from mycroft.api import Api
 from mycroft.messagebus.message import Message
 from mycroft.skills.context import adds_context, removes_context
 from mycroft.skills.core import MycroftSkill, FallbackSkill, \
